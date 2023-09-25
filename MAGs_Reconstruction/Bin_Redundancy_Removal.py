51a8ef753e3e2443afa88a1aa9b4aed1f42a57ad
vishnuprasoodanan <125027228+vishnuprasoodanan@users.noreply.github.com>
2023-09-25 12:17:11 +0200

    Update Bin_Redundancy_Removal.py

--- MAGs_Reconstruction/Bin_Redundancy_Removal.py
+++ MAGs_Reconstruction/Bin_Redundancy_Removal.py
@@ -1,17 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 Created on Sun Sep 24 20:41:38 2023
 
 @author: vishnu
+#Same Script was used with replacing 'Cluster 90' with other clustering criteria (eg: 'Cluster 95', 'Cluster 97' and 'Cluster 99') for selecting cluster-representatives
 """
 import pandas as pd
 import numpy as np
-output_file = 'output.txt'
+output_file = 'Cluster90_represetatives.txt'
 df = pd.read_excel("Final_Bins_Stats_24_09-2023.xlsx", "Sheet1", index_col=0, na_values=["NA"])
 # Get the minimum value in 'Column1'
-min_value = df['Cluster 99'].min()
+min_value = df['Cluster 90'].min()
 
 # Get the maximum value in 'Column1'
-max_value = df['Cluster 99'].max()
+max_value = df['Cluster 90'].max()
 
 with open(output_file, 'a') as file:
     # Get the name of the index column, or provide a custom name if it's not set
@@ -21,7 +24,7 @@ with open(output_file, 'a') as file:
     header_string = '\t'.join([index_column_name] + list(df.columns))
     file.write(header_string + '\n')
     for i in range(1, max_value+1):
-        df_2 = df[df["Cluster 99"].isin([i])]
+        df_2 = df[df["Cluster 90"].isin([i])]
         #print(df_2)
     
         # Find the row with the highest value in Column: completeness
