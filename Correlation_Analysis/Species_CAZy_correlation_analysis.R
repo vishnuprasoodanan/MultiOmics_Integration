89e64581150cf69393d55a7594fc8001719817f2
Vishnu Prasoodanan P K <125027228+vishnuprasoodanan@users.noreply.github.com>
2024-07-29 14:38:18 +0200

    Update Species_CAZy_correlation_analysis.R

--- Correlation_Analysis/Species_CAZy_correlation_analysis.R
+++ Correlation_Analysis/Species_CAZy_correlation_analysis.R
@@ -32,8 +32,8 @@ sanity_check(sub_corr_data)
 
 # Calculate how many times each unique value in the first two columns repeat and print it
 count_repeats <- function(df) {
-  x_counts <- df %>% count(X_features)
-  y_counts <- df %>% count(Y_features)
+  x_counts <- count(df$X_features)
+  y_counts <- count(df$Y_features)
   
   print(x_counts)
   print(y_counts)
@@ -41,6 +41,18 @@ count_repeats <- function(df) {
 
 count_repeats(all_corr_data)
 count_repeats(sub_corr_data)
+
+# Define the function to subset the dataframe
+subset_nonzero <- function(df) {
+  # Remove rows with all elements equal to zero
+  df <- df[rowSums(df != 0) > 0, ]
+  
+  # Remove columns with all elements equal to zero
+  df <- df[, colSums(df != 0) > 0]
+  
+  return(df)
+}
+
 
 # Get unique features
 species <- unique(all_corr_data$X_features)
@@ -85,7 +97,7 @@ if (identical(rownames(all_corr_data_pw_matrix), rownames(all_qval_data_pw_matri
 }
 
 # Step 6: Replace associations where q.values <= 0.05 with zero
-all_corr_data_pw_matrix[all_qval_data_pw_matrix <= 0.05] <- 0
+all_corr_data_pw_matrix[all_qval_data_pw_matrix >= 0.05] <- 0
 
 # Get unique features
 species <- unique(sub_corr_data$X_features)
@@ -130,9 +142,10 @@ if (identical(rownames(sub_corr_data_pw_matrix), rownames(sub_qval_data_pw_matri
 }
 
 # Step 6: Replace associations where q.values <= 0.05 with zero
-sub_corr_data_pw_matrix[sub_qval_data_pw_matrix <= 0.05] <- 0
-
-
+sub_corr_data_pw_matrix[sub_qval_data_pw_matrix >= 0.05] <- 0
+write.table(sub_corr_data_pw_matrix, "sub_corr_data_pw_matrix.txt", sep = "\t", col.names = NA)
+write.table(all_corr_data_pw_matrix, "all_corr_data_pw_matrix.txt", sep = "\t", col.names = NA)
+#----------------------------------------------------------------------------------------------------------------------------
 # Step 2: Check the position of zeros in sub_corr_data_pw_matrix
 zero_positions <- which(sub_corr_data_pw_matrix == 0, arr.ind = TRUE)
 
@@ -150,10 +163,12 @@ for (pos in seq_len(nrow(zero_positions))) {
 
 # Output the new matrix
 print(unique_in_all_corr)
-
+# Subset the dataframe using the function
+subsetted_unique_in_all_corr <- subset_nonzero(unique_in_all_corr)
 # Optionally: Write the new matrix to an output file
-write.table(unique_in_all_corr, "unique_in_all_corr.txt", sep = "\t", col.names = NA)
+write.table(subsetted_unique_in_all_corr, "unique_in_all_corr.txt", sep = "\t", col.names = NA)
 
+#-----------------------------------------------------------------------------------------------------------------------------------------
 # Step 2: Check the position of zeros in sub_corr_data_pw_matrix
 zero_positions2 <- which(all_corr_data_pw_matrix == 0, arr.ind = TRUE)
 
@@ -171,7 +186,73 @@ for (pos in seq_len(nrow(zero_positions2))) {
 
 # Output the new matrix
 print(unique_in_sub_corr)
-
+# Subset the dataframe using the function
+subsetted_unique_in_sub_corr <- subset_nonzero(unique_in_sub_corr)
 # Optionally: Write the new matrix to an output file
-write.table(unique_in_sub_corr, "unique_in_sub_corr.txt", sep = "\t", col.names = NA)
+write.table(subsetted_unique_in_sub_corr, "unique_in_sub_corr.txt", sep = "\t", col.names = NA)
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+# Step 1: Identify positions in sub_corr_data_pw_matrix with values between -5 and 5
+range_positions <- which(sub_corr_data_pw_matrix > -0.5 & sub_corr_data_pw_matrix < 0.5, arr.ind = TRUE)
+
+# Check if range_positions is a matrix and has rows to process
+if (is.matrix(range_positions) && nrow(range_positions) > 0) {
+  # Initialize a new matrix with the same dimensions and names as all_corr_data_pw_matrix filled with zeros
+  increase_in_all_corr <- matrix(0, nrow = nrow(all_corr_data_pw_matrix), ncol = ncol(all_corr_data_pw_matrix))
+  rownames(increase_in_all_corr) <- rownames(all_corr_data_pw_matrix)
+  colnames(increase_in_all_corr) <- colnames(all_corr_data_pw_matrix)
+  
+  # Assign values from all_corr_data_pw_matrix to increase_in_all_corr at the identified positions if they have changed to > 5 or < -5
+  for (pos in seq_len(nrow(range_positions))) {
+    row_idx <- range_positions[pos, 1]
+    col_idx <- range_positions[pos, 2]
+    all_corr_value <- all_corr_data_pw_matrix[row_idx, col_idx]
+    
+    if (all_corr_value > 0.5 || all_corr_value < -0.5) {
+      increase_in_all_corr[row_idx, col_idx] <- all_corr_value
+    }
+  }
+  
+  # Output the new matrix
+  print(increase_in_all_corr)
+  # Subset the dataframe using the function
+  subsetted_increase_in_all_corr <- subset_nonzero(increase_in_all_corr)
+  # Optionally: Write the new matrix to an output file
+  write.table(subsetted_increase_in_all_corr, "Increased_in_all_corr.txt", sep = "\t", col.names = NA)
+} else {
+  message("No positions found in the specified range or invalid input data.")
+}
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+# Step 1: Identify positions in sub_corr_data_pw_matrix with values between -5 and 5
+range_positions2 <- which(sub_corr_data_pw_matrix < -0.5 & sub_corr_data_pw_matrix > 0.5, arr.ind = TRUE)
+
+# Check if range_positions is a matrix and has rows to process
+if (is.matrix(range_positions2) && nrow(range_positions2) > 0) {
+  # Initialize a new matrix with the same dimensions and names as all_corr_data_pw_matrix filled with zeros
+  decrease_in_all_corr <- matrix(0, nrow = nrow(all_corr_data_pw_matrix), ncol = ncol(all_corr_data_pw_matrix))
+  rownames(decrease_in_all_corr) <- rownames(all_corr_data_pw_matrix)
+  colnames(decrease_in_all_corr) <- colnames(all_corr_data_pw_matrix)
+  
+  # Assign values from all_corr_data_pw_matrix to decrease_in_all_corr at the identified positions if they have changed to > 5 or < -5
+  for (pos in seq_len(nrow(range_positions2))) {
+    row_idx <- range_positions2[pos, 1]
+    col_idx <- range_positions2[pos, 2]
+    all_corr_value <- all_corr_data_pw_matrix[row_idx, col_idx]
+    
+    if (all_corr_value < 0.5 || all_corr_value > -0.5) {
+      decrease_in_all_corr[row_idx, col_idx] <- all_corr_value
+    }
+  }
+  
+  # Output the new matrix
+  print(decrease_in_all_corr)
+  # Subset the dataframe using the function
+  subsetted_decrease_in_all_corr <- subset_nonzero(decrease_in_all_corr)
+  # Optionally: Write the new matrix to an output file
+  write.table(subsetted_decrease_in_all_corr, "Decreased_in_all_corr.txt", sep = "\t", col.names = NA)
+} else {
+  message("No positions found in the specified range or invalid input data.")
+}
+
 
